0b82a30f3828da9818fbbc2e44037dc2007dec50
astronaut-lemur <41335987+astronaut-lemur@users.noreply.github.com>
2019-07-18 23:44:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -133,3 +133,13 @@ particlesJS('particles-js',
 );
 
 
+function spaceSchool() {
+  var x = document.getElementById("space_school");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
